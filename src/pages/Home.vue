1555c3100aeb379879f9dbf761562088636997a1
angelcan58 <angelcan58@gmail.com>
2021-03-12 21:06:32 -0600

    se agrego la pagina home

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header/>
+        <Section/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Home/Header.vue'
+import Section from '../components/Home/Section.vue'
+import Footer from '../components/Home/Footer.vue'
+export default {
+    name: 'Home',
+    components: { 
+    Header,
+    Section,
+    Footer 
+    }
+}
+</script>
