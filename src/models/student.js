b1774427d78567203bbea729a798f48f8760f3f1
angelcan58 <angelcan58@gmail.com>
2021-04-15 20:46:57 -0500

    student model has been created

---
+++ src/models/student.js
@@ -0,0 +1,9 @@
+export default class Student {
+    constructor(name, email, course, tutorName, tutorPhone){
+        this.name = name
+        this.email = email
+        this.course = course
+        this.tutorName = tutorName
+        this.tutorPhone = tutorPhone
+    }
+}
