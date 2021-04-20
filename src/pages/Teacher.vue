1fb2b85da85a712a6a819ff2198ba833bc67207b
Enrique Marrufo <knovelo14@gmail.com>
2021-04-19 20:43:28 -0500

    new page created

---
+++ src/pages/Teacher.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Teacher/>
+  </div>
+</template>
+
+<script>
+import Teacher from '../components/Teacher/Teacher.vue'
+
+export default {
+  components: {
+    Teacher
+  }
+}
+</script>
