d2ad4553d6112ff17cd8f0a93d6bbc9e64688722
angelcan58 <angelcan58@gmail.com>
2021-05-02 17:23:55 -0500

    add components

--- src/pages/Teacher.vue
+++ src/pages/Teacher.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <Searchbar/>
     <Teacher/>
   </div>
 </template>
 
 <script>
 import Teacher from '../components/Teacher/Teacher.vue'
+import Searchbar from '../components/searchbar/searchbar.vue'
 
 export default {
   components: {
+    Searchbar,
     Teacher
   }
 }
