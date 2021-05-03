281d2db9e474cd366eadcf2208598218fe041e41
angelcan58 <angelcan58@gmail.com>
2021-05-02 20:01:39 -0500

    change button logout

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -14,7 +14,7 @@
         </nav>
         <nav class="nav-button" v-if="currentUser">
             <router-link to="/signUp" v-if="currentUser.rol == 'admin'" class="btn-area">{{ route5 }}</router-link>
-            <b-button @click.prevent="logOut" variant="outline-info" class="mb-2">
+            <b-button @click.prevent="logOut" class="btn-area" id="cerrar">
                 <b-icon icon="box-arrow-in-right" aria-hidden="true"></b-icon>Cerrar Sesion
             </b-button>
         </nav>
@@ -107,5 +107,8 @@ export default {
 }
 .nav-button{
     padding: 0px 20px;
+}
+#cerrar{
+    font-size: 13px;
 }
 </style>
