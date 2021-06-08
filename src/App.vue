735b1652235f3da48563f802b601fe91f6532eb6
angelcan58 <angelcan58@gmail.com>
2021-06-08 14:41:20 -0500

    remove footer

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
   <div id="app">
     <Navbar route1="Inicio" route2="About" route3="Contact" route4="LogIn" route5="Agregar nuevo" route6="Dashboard" route7="cerrar sesion"/>
     <router-view/>
-    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar/Navbar.vue'
-import Footer from './components/Home/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar,
-    Footer
+    Navbar
   }
 }
 </script>
